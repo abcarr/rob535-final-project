18821e9f7704c43869fdb84bc598e0d04dd90d7c
Alexander Carr <abcarr@umich.edu>
2025-11-29 22:29:55 -0500

    update pdm path

--- scripts/miscs/gen_pdm_score.sh
+++ scripts/miscs/gen_pdm_score.sh
@@ -1,8 +1,8 @@
-python scripts/misc/gen_multi_trajs_pdm_score.py \
+python scripts/miscs/gen_multi_trajs_pdm_score.py \
 agent=WoTE_agent \
-'agent.checkpoint_path="/home/yingyan.li/repo/WoTE/exp/WoTE/default/lightning_logs/version_0/checkpoints/epoch=29-step=9990.ckpt"' \
+'agent.checkpoint_path="/scratch/rob535f25s001_class_root/rob535f25s001_class/abcarr/rob535-final-project/exp/WoTE/default/lightning_logs/version_0/checkpoints/epoch=29-step=9990.ckpt"' \
 agent.config._target_=navsim.agents.WoTE.configs.default.WoTEConfig \
 experiment_name=eval/gen_data \
-metric_cache_path='/data2/yingyan_li/repo/WoTE/dataset/metric_cache/trainval' \
+metric_cache_path='/scratch/rob535f25s001_class_root/rob535f25s001_class/abcarr/rob535-final-project/exp/metric_cache/trainval' \
 split=trainval \
 scene_filter=navtrain \
