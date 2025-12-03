701a9656e54253f03aeb92fbaa6fae69f50df12c
Vidya Marri <vmarri@ford.com>
2025-12-02 23:04:16 -0500

    config and yaml to false

--- navsim/agents/WoTE/configs/default.py
+++ navsim/agents/WoTE/configs/default.py
@@ -65,7 +65,7 @@ class WoTEConfig:
     detect_boxes = False
     use_bev_semantic = False
 
-    use_semantic: bool = True  # Required for multitask learning (image features from backbone)
+    use_semantic: bool = False  # Required for multitask learning (image features from backbone)
     use_depth: bool = False
     add_features: bool = True
 
